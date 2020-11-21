ce29b21394ce7a07dbd095b763d99cf2cff90864
xternet <majewski.post@gmail.com>
2020-11-21 14:16:27 +0100

    .

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,16 +5,16 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow text-monospace">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
           href="http://www.dappuniversity.com/bootcamp"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Dapp University Supply Chain
+          <img src={logo} width="30" height="30" className="align-top" alt="" />
+          &nbsp;$upply_Ch@in
         </a>
-
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
             <small className="text-secondary">
